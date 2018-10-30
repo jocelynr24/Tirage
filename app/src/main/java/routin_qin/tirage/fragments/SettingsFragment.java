88ddbfbf7e637248ba7fb3b83568f4de47c6ca6f
Jocelyn <jocelyn.routin@outlook.fr>
2018-10-30 09:31:13 +0100

    Ajout du choix de la langue

--- app/src/main/java/routin_qin/tirage/fragments/SettingsFragment.java
+++ app/src/main/java/routin_qin/tirage/fragments/SettingsFragment.java
@@ -1,11 +1,19 @@
 package routin_qin.tirage.fragments;
 
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Locale;
 
 import routin_qin.tirage.R;
 
@@ -14,7 +22,9 @@ import routin_qin.tirage.R;
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
-
+    View myView;
+    String[] arraySpinner;
+    String spinner_lang;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -22,10 +32,57 @@ public class SettingsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        //return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        myView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        this.arraySpinner = new String[] {
+                myView.getContext().getString(R.string.settings_en), myView.getContext().getString(R.string.settings_fr)
+        };
+        final Spinner mySpinner = (Spinner) myView.findViewById(R.id.spinner_lang);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, arraySpinner);
+        mySpinner.setAdapter(adapter);
+
+        final Button button = myView.findViewById(R.id.bt_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                spinner_lang = mySpinner.getSelectedItem().toString();
+                changeLanguage(getResources(), spinner_lang);
+            }
+        });
+
+        return myView;
+    }
+
+
+    private void changeLanguage(Resources res, String lang) {
+        Configuration config;
+        config = new Configuration(res.getConfiguration());
+
+        switch(lang){
+            case "English":
+                config.locale = new Locale("en");
+                break;
+            case "Anglais":
+                config.locale = new Locale("en");
+                break;
+            case "French":
+                config.locale = new Locale("fr");
+                break;
+            case "Français":
+                config.locale = new Locale("fr");
+                break;
+            default:
+                config.locale = new Locale("en");
+                break;
+        }
+
+        getResources().updateConfiguration(config, res.getDisplayMetrics());
+        Intent i = myView.getContext().getPackageManager().getLaunchIntentForPackage( myView.getContext().getPackageName() );
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 
 }
